472e3ecb47e7ede5dde20e8cf4994faf56fe4507
Diego Wilson <diego.wilson@uda.cl>
2018-01-25 20:47:31 -0300

    Directorios creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { NopagefoundComponent } from './component/nopagefound/nopagefound.component';
+import { DashboardComponent } from './component/pages/dashboard/dashboard.component';
+import { Graficas1Component } from './component/pages/graficas1/graficas1.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { SidebarComponent } from './component/shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './component/shared/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
